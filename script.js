7e11f239f7dc4f0fa039341296019e58495e7b0f
blubluecori <camila.kawaii.2004@gmail.com>
2025-07-14 23:10:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,48 @@ const datosMalla = [
       { nombre: "Lenguaje y comunicación", tipo: "teorico" },
     ]
   },
-  
+  {
+    semestre: "Semestre 3",
+    ramos: [
+      { nombre: "Mundo moderno XVI-XVIII", tipo: "teórico" },
+      { nombre: "América colonial XVI-XVIII", tipo: "teórico" },
+      { nombre: "Chile colonial XVII-XVIII", tipo: "teórico" },
+      { nombre: "Teoría del pensamiento económico", tipo: "teórico" },
+      { nombre: "Gestión en el aula", tipo: "teórico" },
+      { nombre: "Ética y educación", tipo: "teórico" }
+    ]
+  },
+  {
+    semestre: "Semestre 4",
+    ramos: [
+      { nombre: "Mundo contemporáneo XVIII-XIX", tipo: "teórico" },
+      { nombre: "América siglo XIX", tipo: "teórico" },
+      { nombre: "Chile siglo XIX", tipo: "teórico" },
+      { nombre: "Geografía física y problemáticas medioambientales", tipo: "teórico" },
+      { nombre: "Teoría del aprendizaje en el contexto de la adolescencia y cultura juvenil", tipo: "teórico" },
+      { nombre: "Práctica inicial", tipo: "práctico" }
+    ]
+  },
+  {
+    semestre: "Semestre 5",
+    ramos: [
+      { nombre: "Mundo contemporáneo XX", tipo: "teórico" },
+      { nombre: "América siglo XX", tipo: "teórico" },
+      { nombre: "Chile siglo XX", tipo: "teórico" },
+      { nombre: "Ciudadanía, democracia y derechos humanos", tipo: "teórico" },
+      { nombre: "Gestión curricular para el aprendizaje", tipo: "teórico" },
+      { nombre: "Métodos de evaluación educativa", tipo: "teórico" },
+      { nombre: "Taller de integración 1", tipo: "práctico" }
+    ]
+  },
+  {
+    semestre: "Semestre 6",
+    ramos: [
+      { nombre: "Historia de Estados Unidos y América Latina", tipo: "teórico" },
+      { nombre: "Teoría de la historia geográfica de la población", tipo: "teórico" },
+      { nombre: "Planificación curricular y evaluación", tipo: "teórico" },
+      { nombre: "Didáctica de la historia", tipo: "teórico" },
+      { nombre: "Taller de integración 2", tipo: "práctico" }
   },
   {
     semestre: "Semestre 7",
